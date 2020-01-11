0d7a0e81f38bdeeeb34725ea395e2248aa9f4764
github username <github email address>
2020-01-11 14:34:16 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+require "pry"
+class Anagram
+    attr_accessor :setter, :getter
+
+    def initialize(setter)
+        @setter = setter
+    end 
+
+    def match(getter)
+        setter_letters=setter.split('')
+        setter_letters.sort!
+        i = 0
+        a = []
+        while i < getter.length
+            i_array = []
+            i_array << getter[i]
+            join = i_array.join('').split('').sort
+            if join == setter_letters
+            a << getter[i]
+            end 
+            i += 1
+        end 
+        a
+    end 
+
+end 
