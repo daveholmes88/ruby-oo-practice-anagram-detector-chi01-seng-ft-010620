bc597ef80e77035d3a11f62106096448e5b8a7e8
github username <github email address>
2020-01-11 14:36:23 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,20 +7,21 @@ class Anagram
     end 
 
     def match(getter)
-        setter_letters=setter.split('')
-        setter_letters.sort!
-        i = 0
-        a = []
-        while i < getter.length
-            i_array = []
-            i_array << getter[i]
-            join = i_array.join('').split('').sort
-            if join == setter_letters
-            a << getter[i]
-            end 
-            i += 1
-        end 
-        a
+        # setter_letters=setter.split('')
+        # setter_letters.sort!
+        # i = 0
+        # a = []
+        # while i < getter.length
+        #     i_array = []
+        #     i_array << getter[i]
+        #     join = i_array.join('').split('').sort
+        #     if join == setter_letters
+        #     a << getter[i]
+        #     end 
+        #     i += 1
+        # end 
+        # a
+        getter.select {|x| x.split("").sort == @setter.split("").sort}
     end 
 
 end 
